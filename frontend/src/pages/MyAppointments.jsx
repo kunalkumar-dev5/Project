fee0fcafdd10edd3737334a736f747fd8e483534
Kunal Kumar <kunalkr5674@gmail.com>
2026-06-03 23:20:04 +0530

    Built MyAppointment section

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const MyAppointments = () => {
+
+  const { doctors } = useContext(AppContext)
+
   return (
     <div>
-    
+      <p>My Appointments</p>
+      <div>
+        {doctors.slice(0, 2).map((item, index) => (
+          <div key={index}>
+            <div>
+              <img src={item.image} alt="" />
+            </div>
+            <div>
+              <p>{item.name}</p>
+              <p>{item.speciality}</p>
+              <p>Address:</p>
+              <p>{item.address.line1}</p>
+              <p>{item.address.line2}</p>
+              <p><span>Date & Time:</span> 3, june, 2026 | 8:00 PM</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
     </div>
   )
 }
