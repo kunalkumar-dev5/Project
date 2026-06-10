6f16fb2de3ddcb3056bc27898f373817a8059a67
Kunal Kumar <kunalkr5674@gmail.com>
2026-06-10 22:04:47 +0530

    debug

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-
     mongoose.connection.on('connected', () => console.log("Database Connected"));
 
-    await mongoose.connect(`${process.evn.MONGODB_URI}/project`)
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/project`)
 };
 
 export default connectDB
