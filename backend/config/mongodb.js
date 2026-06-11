ccb27ddd94ae55e6bd010fcdea717b81caaa01bf
Kunal Kumar <kunalkr5674@gmail.com>
2026-06-11 17:39:38 +0530

    fix bug

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,10 +1,36 @@
 import mongoose from "mongoose";
 
+// const connectDB = async () => {
+//     mongoose.connection.on('connected', () => console.log("Database Connected"));
+//     mongoose.connection.on('error', (err) => console.error("Database Connection Error:", err.message));
+
+//     const mongoUri = process.env.MONGODB_URI;
+
+//     if (!mongoUri) {
+//         console.error("MONGODB_URI is not defined in .env");
+//         return;
+//     }
+
+//     try {
+//         await mongoose.connect(mongoUri, {
+//             dbName: "project",
+//             serverSelectionTimeoutMS: 10000,
+//         });
+//     } catch (error) {
+//         console.error("MongoDB connection failed:", error.message);
+//     }
+// };
+
+// export default connectDB
+
+
 const connectDB = async () => {
-    mongoose.connection.on('connected', () => console.log("Database Connected"));
+    console.log("URI =", process.env.MONGODB_URI);
 
+    mongoose.connection.on('connected', () =>
+        console.log("Database Connected")
+    );
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/project`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`);
 };
-
-export default connectDB
+export default connectDB;
