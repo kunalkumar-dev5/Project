db965c2b45d9aa9f2a2896991ac7693c68299bfc
Kunal Kumar <kunalkr5674@gmail.com>
2026-06-13 17:06:18 +0530

    update new features

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+// API FOR ADDING DOCTOR
+const addDoctor = async (req, res) => {
+    try {
+        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body;
+    }catch (error) {
+    }
+}
+
+export {addDoctor}
