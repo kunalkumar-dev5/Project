82ee96d3baf6fe2f274e618d63331ff36c4140cc
Kunal Kumar <kunalkr5674@gmail.com>
2026-06-27 14:10:40 +0530

    fix backend login page

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,22 +1,58 @@
 import React, { useState } from "react"
+import { assets } from '../assets/assets'
+import { AdminContext } from "../context/AdminContext"
+import axios from "axios"
 
 const Login = () => {
     const [state, setState] = useState('Admin')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const { setAToken, backendUrl } = React.useContext(AdminContext)
+
+    const onSubmitHandler = async (event) => {
+
+        event.preventDefault()
+
+        try {
+            if (state === 'Admin') {
+                   const {data} = await axios.post(backendUrl + '/admin/login', { email, password })
+               if(data.success){
+                console.log(data.token)
+                setAToken(data.token)
+               }
+            } else {
+
+
+            }
+
+        } catch (error) {
+
+
+         }
+    }
 
     return (
-        <form className='min-h-[80vh] flex items-center'>
+        <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
             <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-sm'>
                 <p className='text-2xl font-semibold m-auto'> <span className='text-primary'>{state}</span> Login</p>
                 <div className='w-full'>
                     <p>Email</p>
-                    <input className="border border-[#DADADA] rounded w-full p-2 mt-1 focus:outline-none focus:ring-2 focus:ring-primary" type="email" required />
+                    <input
+                        className="border border-[#DADADA] rounded w-full p-2 mt-1 focus:outline-none focus:ring-2 focus:ring-primary"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
                 </div>
                 <div className="w-full">
                     <p>Password</p>
                     <input
                         className="border border-[#DADADA] rounded w-full p-2 mt-1 focus:outline-none focus:ring-2 focus:ring-primary"
                         type="password"
-                        autoComplete="current-password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                         required
                     />
                 </div>
