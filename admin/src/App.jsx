998fdd81f81ef3b69b6247406e412ffa5b24efdf
Kunal Kumar <kunalkr5674@gmail.com>
2026-06-30 21:30:21 +0530

    add Sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,6 +5,12 @@ import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext'
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import AllApointments from './pages/Admin/AllApointments';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorsList from './pages/Admin/DoctorsList';
+
 
 const App = () => {
 
@@ -16,6 +22,13 @@ const App = () => {
       <Navbar />
       <div className='flex items-start' >
         <Sidebar />
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllApointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctors-list' element={<DoctorsList />} />
+        </Routes>
       </div>
     </div>
   ) : (
