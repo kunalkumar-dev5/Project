f59d963216cf9eb4d8bc6a88b6a619e7482032a5
Kunal Kumar <kunalkr5674@gmail.com>
2026-07-08 19:49:58 +0530

    add Doctore list

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+
+import { AdminContext } from '../../context/AdminContext'
+import React,{ useContext, useEffect } from 'react'
 
 const DoctorsList = () => {
+  const { doctors, aToken, getAllDoctors } = useContext(AdminContext)
+
+  useEffect(() => {
+    if (aToken) {
+      getAllDoctors()
+    }
+  }, [aToken])
+
   return (
     <div>
-      
+
     </div>
   )
 }
